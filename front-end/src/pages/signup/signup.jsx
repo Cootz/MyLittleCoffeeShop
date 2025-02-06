c4b52926ed286c22dfed2fe3a47e2e111f912bbb
Cootz <artem2004799@gmail.com>
2025-02-06 18:12:37 +0300

    Make Signup page layout

--- front-end/src/pages/signup/signup.jsx
+++ front-end/src/pages/signup/signup.jsx
@@ -5,6 +5,11 @@ export default function SingUp()
     return (
         <div className="signup-container">
             <h1>Sign Up</h1>
+            <input type="text" placeholder="Email"/>
+            <input type="text" placeholder="Username"/>
+            <input type="password" placeholder="Password"/>
+            <input type="password" placeholder="Confirm Password"/>
+            <button className="cs-button">Sign Up</button>
         </div>
     );
 }
